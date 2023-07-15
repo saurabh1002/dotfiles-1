52e8f884d058d5ef9b9b800209f0dc4cdeef8e13
l00p3 <luca.lobefaro95@gmail.com>
2023-07-15 19:52:18 +0200

    nvim plug install in installation script

---
+++ my_conf_install.sh
@@ -0,0 +1,19 @@
+# Install packages
+sudo pacman -S zsh tmux kitty
+
+# Install oh-my-zsh and plugins
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Make zsh default shell
+chsh -s /bin/zsh
+
+# Install tmuxinator
+
+# Clone my dotfiles
+git clone --bare git@github.com:l00p3/dotfiles.git
+
+# Install nvim plugins
+url -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall +qall
